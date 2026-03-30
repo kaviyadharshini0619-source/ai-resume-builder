cf78173e7c1c75497c8dedcbff47d88488b673f4
kaviyadharshini0619-source <kaviyadharshini0619@gmail.com>
2026-03-31 00:32:54 +0530

    render port fix

--- app.py
+++ app.py
@@ -178,5 +178,8 @@ learning new technologies and applying problem-solving skills to real-world proj
     return send_file(file_path, as_attachment=True)
     
 # Run server
-if __name__ == '__main__':
-    app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
